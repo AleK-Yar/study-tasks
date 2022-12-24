ad1f881042f8d46c3add5e949c7b8771823a913f
AleK-Yar <alek-yar@hotmail.com>
2022-12-24 11:41:41 +0300

    Rename file bitlink.py

--- Devman/bitlink.py
+++ Devman/bitlink.py
@@ -4,12 +4,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
-
-bitlinks = 'https://bit.ly/3YKOIlW'  # 'https://bit.ly/3BWoIKq', 'https://bit.ly/3hNTr5I'
-long_url = 'https://obuchalka.org/20200217118457/sekreti-python-59-rekomendacii-po-napisaniu-effektivnogo-koda-slatkiv-b-2016.html'
-token = os.environ['TOKEN']
-domain = 'bit.ly'
+TOKEN = os.environ['TOKEN']
 
 
 def shorten_link(token, link):
@@ -19,11 +14,7 @@ def shorten_link(token, link):
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1'
     }
-    data = {
-        "long_url": link,
-        "domain": domain,
-        "group_guid": ""
-    }
+    data = {"long_url": link}
 
     check_link = requests.get(link, headers=headers)
     check_link.raise_for_status()
@@ -43,24 +34,23 @@ def count_clicks(token, bitlink):
         'Referer': 'https://www.google.com/',
     }
     params = (
-        ('unit', ''),
-        ('units', '-1'),
-        ('unit_reference', ''),
+            ('unit', ''),
+            ('units', '-1'),
+            ('unit_reference', ''),
     )
 
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
-    # pprint(response.json())
     total_clicks = response.json()["total_clicks"]
     return total_clicks
 
 
 def is_bitlink(url):
     parsed = urlparse(url)
-    return parsed.netloc == domain
+    return parsed.netloc == 'bit.ly'
 
 
-def cut_shema_bitlink(url):
+def cut_sсhema_bitlink(url):
     parsed = urlparse(url)
     return ''.join(parsed[1:])
 
@@ -69,10 +59,10 @@ def main():
     user_input = input('Введите ссылку: ')
     try:
         if is_bitlink(user_input):
-            count = count_clicks(token, cut_shema_bitlink(user_input))
+            count = count_clicks(TOKEN, cut_sсhema_bitlink(user_input))
             print(f'По вашей ссылке, прошли {count} раз(а)')
         else:
-            bitlink = shorten_link(token, user_input)
+            bitlink = shorten_link(TOKEN, user_input)
             print('Битлинк:', bitlink)
     except requests.exceptions.RequestException as err:
         print('Ошибка при проверке ссылки:', err)
@@ -80,5 +70,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
