ddba9ec6ad03ed8bf9cdb2ff936c1b02f9b05c46
AleK-Yar <alek-yar@hotmail.com>
2023-01-19 17:10:09 +0300

    Add lesson_1_3_4.py

---
+++ Good_good_Python_OOP_Stepik(116336)/lesson_1_3_4.py
@@ -0,0 +1,9 @@
+"""https://stepik.org/lesson/701973/step/4?unit=702074"""
+
+
+class DataBase:  # объявляем класс DataBase с атрибутами (pk, title, author, views и comments)
+    pk = 1
+    title = 'Классы и объекты'
+    author = 'Сергей Балакирев'
+    views = 14356
+    comments = 12
